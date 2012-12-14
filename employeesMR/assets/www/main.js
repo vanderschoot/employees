c4bbf4d25d35b85763271a886726c89d676ee767
Joost van der Schoot <joostvdschoot@hotmail.com>
2012-12-14 19:54:04 +0100

    poort aangepast

--- employeesMR/assets/www/main.js
+++ employeesMR/assets/www/main.js
@@ -1,4 +1,4 @@
 var serverhost = "10.0.2.2"; // Special alias to localhost loopback interface
-var serverport = "8082";
-var serverproject = "employeesREST";
+var serverport = "8080";
+var serverproject = "employeesRESTH";
 var serverurl = "http://" + serverhost + ":" + serverport + "/" + serverproject;
